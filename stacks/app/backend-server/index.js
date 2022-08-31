d3930ed961818286b148fc5b6883cbe6f8f2083f
Mikhail Evseev <mikhail.yevseev@gmail.com>
2022-09-01 00:15:21 +0800

    logs

--- stacks/app/backend-server/index.js
+++ stacks/app/backend-server/index.js
@@ -4,8 +4,8 @@ const port = 80
 
 app.get('/api/environment', (req, res) => {
     res.send(req.header('X-Env'));
-})
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!..`);
-})
+});
